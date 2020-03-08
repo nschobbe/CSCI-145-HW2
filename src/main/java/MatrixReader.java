71ecc2324c5f7dcfa59276869825565333e7d825
Grace Valdez <valdezg@wwu.edu>
2020-03-07 16:46:13 -0800

    matrix reader hopefully

--- src/main/java/MatrixReader.java
+++ src/main/java/MatrixReader.java
@@ -1,15 +1,40 @@
 import java.util.Scanner;
-import java.io.File;
 
 public class MatrixReader {
 
-    //should read rows and columns, then init a SparseMatrix object with those values
-    //then, iterarte through the file row by row, inserting ValueNodes at the correct places
-    //you will only ever have to insert to the end of any column list, but the order in the row lists is fucky
-    //if you have two ValueNodes (a and b) and a third (c) needs to go between them, put b in a temp, 
-    //break the reference from a to b
-    //new reference goes a to c, then c.setNext(to the temp)
-    public SparseMatrix read(String file) {
+    public SparseMatrix read(String file){
+
+        SparseMatrix matrix = null;
+
+        try {
+            File matrixFile = new File("matrixA.txt");
+            Scanner input = new Scanner(matrixFile);
+
+            //Consider using input.nextLine b/c might try to parse /n which might throw errors so just check when testing.
+            int rows = Integer.parseInt(input.next()); //parse the first input into an integer defining total rows.
+            int columns = Integer.parseInt(input.next()); // parse the second input into an integer defining total columns
+
+            matrix = new SparseMatrix(rows, columns); //create sparce matrix
+            int counter = 1; //counter for which row we are on
+
+            while (input.hasNext()) {                            //While there is more in the text file: make a string array of the token created
+                String[] token = (input.nextLine()).split(" "); //by splitting the first line with spaces. (1,8)
+
+                for (i = 0; i < token.length(); i++) {           //For every token in the array split them with commas and place them
+                    String[] finalTokens = token[i].split(","); //in a new array called finalTokens.
+
+                    int columnNumber = Integer.parseInt(finalTokens[0]); //parse finaltokens for integer at index 0.
+                    int value = Integer.parseInt(finalTokens[1]); //parse final tokens for integer at index 1.
+                    matrix.insert(counter, columnNumber, value); //call matrix.insert with counter for row #, col #, and value
+                    counter = counter + 1;
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not found.");
+        }
+
+        return matrix;
     }
-        
 }
+//hopefully this works
