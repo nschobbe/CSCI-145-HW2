9bec686be0d219def4e9b918bf694ad920ee2e02
--nschobbe <schobbn@wwu.edu>
2020-03-10 16:24:38 -0700

    matrixrow insert method finished

--- src/main/java/MatrixColumn.java
+++ src/main/java/MatrixColumn.java
@@ -16,7 +16,30 @@ public class MatrixColumn {
     }
 
     public void insert(ValueNode value) { // responsible for inserting values into the list in sorted order
-        
+        if (this.first == null) { //if the row is empty, set the first equal to the valuenode being inserted
+            this.first = value;
+        }
+        else if (this.first.getRow() < value.getRow()) { //if the col of the value being inserted is less than the col of the first, put the val in before
+            ValueNode temp = this.first;
+            this.first = value;
+            this.first.setNextRow(temp);
+        }
+        else if (this.first.getRow() > value.getRow()) { //else insert it in the correct spot
+            ValueNode temp = this.first;
+            ValueNode temp2;
+            while (value.getRow() > temp.getRow() && temp.getNextRow() != null) {
+                temp = temp.getNextRow();
+            }
+            if (temp.getNextRow() != null) {
+                temp2 = temp.getNextRow();
+                temp.setNextRow(value);
+                temp = temp.getNextRow();
+                temp.setNextRow(temp2);
+            }
+            else {
+                temp.setNextRow(value);
+            }
+        }
     
     }
 
